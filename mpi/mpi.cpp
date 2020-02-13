6762ccb00fe74283e1e5eb44fdb36f318f671267
Rami Hansen <rami.hansen2@gmail.com>
2020-02-12 21:13:36 -0800

    mpi distributed processing working

--- mpi/mpi.cpp
+++ mpi/mpi.cpp
@@ -6,18 +6,30 @@
 #include "../EasyBMP.h"
 
 void mpi_master(BMP&, State&);
-void mpi_slave();
+void mpi_slave(BMP&, State&);
 
+// used for transmitting a processed pixel back to the master process
+#pragma pack(2)
+struct mpi_pixel {
+	int x {0};
+	int y {0};
+	int red;
+	int green;
+	int blue;
+};
+#pragma pack()
+
+
+// Each process opens their own copy of the bitmap file.
+// Slave processes send processed pixels back to the master
+// process to be compiled into a complete image and saved.
 
 int main(int argc, char* argv[]) {
 	int ierr = MPI_Init(&argc, &argv);
 
 	int id;
-    
-	MPI_Comm_rank(MPI_COMM_WORLD, &id);
+    MPI_Comm_rank(MPI_COMM_WORLD, &id);
 	
-
-
 	long time {0};
 
 	if(id == 0) {
@@ -41,7 +53,16 @@ int main(int argc, char* argv[]) {
 		std::cout << "Time for distributed algorithm to complete: " << time << "ms" << std::endl;
 	}
 	else {
-		mpi_slave();
+		// This is a slave process
+		State s;
+		if(!s.bmp.ReadFromFile(argv[1])) {
+			return 1;
+		}
+
+		s.kern_process = static_cast<Kernel_type>(std::stoi(argv[2]));
+
+		BMP output_file = s.bmp;
+		mpi_slave(output_file, s);
 	}
     ierr = MPI_Finalize();
 	return static_cast<int>(time);
@@ -52,12 +73,6 @@ void mpi_master(BMP& output_file, State& s) {
 	MPI_Comm_size(MPI_COMM_WORLD, &process_count);
 
 	int interval = output_file.TellHeight()/(process_count-1);
-
-	// send each slave the state and input bmp
-	// for(int i = 1; i < process_count; i++) {
-	// 	MPI_Send(&output_file, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-	// 	MPI_Send(&s, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-	// }
 
 	// send each slave its processing segment
 	int start_row;
@@ -74,18 +89,23 @@ void mpi_master(BMP& output_file, State& s) {
 	end_row = 0;
 	MPI_Send(&start_row, 1, MPI_INT, process_count-1, 0, MPI_COMM_WORLD);
 	MPI_Send(&end_row, 1, MPI_INT, process_count-1, 0, MPI_COMM_WORLD);
+
+
+	// recv mpi pixel structs from anywhere
+	for(int y = 0; y < output_file.TellHeight(); y++) {
+		for(int x = 0; x < output_file.TellWidth(); x++) {
+			// recv
+			mpi_pixel recv_pixel;
+			MPI_Recv(&recv_pixel, 5, MPI_INT, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			RGBApixel* p = output_file(recv_pixel.x, recv_pixel.y);
+			p->Red = static_cast<u_int8_t>(recv_pixel.red);
+			p->Green = static_cast<u_int8_t>(recv_pixel.green);
+			p->Blue = static_cast<u_int8_t>(recv_pixel.blue);
+		}
+	}
 }
 
-void mpi_slave() {
-	BMP* output_file_point;
-	// receive output_file address
-	// MPI_Recv(&output_file_point, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-	BMP& output_file = *output_file_point;
-
-	State* state_point;
-	// receive state address - maybe figure out how to send/receive entire data
-	// MPI_Recv(&state_point, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-	State& state = *state_point;
+void mpi_slave(BMP& output_file, State& s) {
 
 	int start_row;
 	MPI_Recv(&start_row, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
@@ -93,7 +113,29 @@ void mpi_slave() {
 	int end_row;
 	MPI_Recv(&end_row, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-	std::cout << "Slave received start_row: " << start_row << " end_row: " << end_row << std::endl;
-	
-	// kernel_process(output_file, state, start_row, end_row);
+
+	// kernel processing
+	kernel_process(output_file, s, start_row, end_row);
+
+	// set the end row, works better for the send loop
+	if(end_row == 0) {
+		end_row = output_file.TellHeight();
+	}
+
+	// send pixel data to master process
+	for(int y = start_row; y < end_row; y++) {
+		for(int x = 0; x < output_file.TellWidth(); x++) {
+			RGBApixel* p = output_file(x, y);
+			mpi_pixel send_pixel;
+			send_pixel.x = x;
+			send_pixel.y = y;
+			send_pixel.red = p->Red;
+			send_pixel.green = p->Green;
+			send_pixel.blue = p->Blue;
+
+			// send the struct as if it were an array of ints
+			MPI_Send(&send_pixel, 5, MPI_INT, 0, 0, MPI_COMM_WORLD);
+		}
+	}
+	std::cout << "slave finished sending" << std::endl;
 }
